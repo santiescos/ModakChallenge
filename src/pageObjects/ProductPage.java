7218c4c239a2c07594bbe50fae4953c9db439de5
santiescos <santiescosteguy@gmail.com>
2024-10-14 17:22:14 -0300

    created method to extract the numeric value of the available products, and returning a boolean that confirms that is greater than 0

--- src/pageObjects/ProductPage.java
+++ src/pageObjects/ProductPage.java
@@ -1,12 +1,21 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductPage {
+	private final By elementWithInfoFromAvailability = By.cssSelector("div[class='quantity--info--jnoo_pD'] div span");
 	WebDriver driver;
 	
 	public ProductPage(WebDriver driver) {
 		this.driver = driver;
 	}
-
+	
+	public boolean getNumberFromElement() {
+		String stringFromElement = driver.findElement(elementWithInfoFromAvailability).getText();
+		String[] parts = stringFromElement.split(" ");
+        int availableProducts = Integer.parseInt(parts[0]);
+        return availableProducts !=0;
+	}
+		
 }
