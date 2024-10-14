a2c6f4f0ee67b71d2a6dd5c0b2e46e205eed6990
santiescos <santiescosteguy@gmail.com>
2024-10-14 16:04:29 -0300

    Defined locator to work in ResultsPage, and created methods to convert the locator to a list, to click on second element of the list and to change the window to the opened from clicking the second result

--- src/pageObjects/ResultsPage.java
+++ src/pageObjects/ResultsPage.java
@@ -1,12 +1,26 @@
 package pageObjects;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ResultsPage {
+	private final By products = By.cssSelector("div[class='list--gallery--C2f2tvm search-item-card-wrapper-gallery']");
 	WebDriver driver;
 	
 	public ResultsPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
+	private List<WebElement> convertSelectorToArray() {
+		return driver.findElements(products);
+	}
+	
+	
+	public void clickOnSecondProduct() {
+		convertSelectorToArray().get(1).click();
+	}
 }
