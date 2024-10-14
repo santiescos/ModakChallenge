f4b9194dc8deef078d767b84721e8c0179e991ba
santiescos <santiescosteguy@gmail.com>
2024-10-14 15:50:48 -0300

    Defined locators and product to be used in landingpage

--- src/pageObjects/LandingPage.java
+++ src/pageObjects/LandingPage.java
@@ -1,8 +1,13 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LandingPage {
+	private final String productToSearch = "instax mini";
+	private final By searchBox = By.id("search-words");
+	private final By iconToSubmitSearch = By.cssSelector("input[class='search--submit--2VTbd-T']");
+	
 	WebDriver driver;
 	
 	public LandingPage(WebDriver driver) {
