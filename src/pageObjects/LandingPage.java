76568719d86bf268bd4e1aaf54aa3f2f440641c8
santiescos <santiescosteguy@gmail.com>
2024-10-14 15:52:30 -0300

    Added method to send keys to the searchbox and click to search

--- src/pageObjects/LandingPage.java
+++ src/pageObjects/LandingPage.java
@@ -6,12 +6,15 @@ import org.openqa.selenium.WebDriver;
 public class LandingPage {
 	private final String productToSearch = "instax mini";
 	private final By searchBox = By.id("search-words");
-	private final By iconToSubmitSearch = By.cssSelector("input[class='search--submit--2VTbd-T']");
-	
+	private final By iconToSubmitSearch = By.cssSelector("input[class='search--submit--2VTbd-T']");	
 	WebDriver driver;
 	
 	public LandingPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
+	public void searchProduct() {
+		driver.findElement(searchBox).sendKeys(productToSearch);
+		driver.findElement(iconToSubmitSearch).click();
+	}
 }
